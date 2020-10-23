2e53028d91a9257c2093be0c532b09f3ffc79839
Pushpakant Behera <pushpakantbehera@hotmail.com>
2020-10-23 22:54:19 +0545

    extracted data from form

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,34 +1,65 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    public AnchorPane draggableArea;
+    @FXML
+    private AnchorPane draggableArea;
+
     double xOffset=0;
     double yOffset=0;
 
-    public Line divider;
-    public JFXButton loginBtn;
-    public Button closeBtn;
-    public Button minimizeBtn;
+    @FXML
+    private Button closeBtn;
 
+    @FXML
+    private JFXTextField fullName;
+
+    @FXML
+    private JFXTextField fatherName;
+
+    @FXML
+    private  JFXTextField motherName;
+
+    @FXML
+    private JFXRadioButton loginBtn;
+
+    @FXML
+    private JFXRadioButton registerBtn;
+
+    @FXML
+    private JFXRadioButton generalBtn;
+
+    @FXML
+    private  JFXRadioButton reservationBtn;
+
+    @FXML
+    private JFXRadioButton maleBtn;
+
+    @FXML
+    private JFXRadioButton femaleBtn;
+
+    @FXML
+    private JFXTextField rollOrIdBtn;
+
+    @FXML
+    private JFXPasswordField passwordBtn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        //  -------------------------------- change position later of divider
-        divider.setStartX(Main.height/2);
-        divider.setStartY(100);
-        divider.setEndX(Main.height/2);
-        divider.setEndY(Main.width-100);
 
         //  making the custom title bar dragable
         draggableArea.setOnMousePressed(e->{
@@ -44,6 +75,28 @@ public class Controller implements Initializable {
             stage.setX(e.getScreenX() + xOffset);
             stage.setY(e.getScreenY() + yOffset);
         });
+    }
+
+    public void loginBtnClick(){
+        String id = rollOrIdBtn.getText();
+        String pwd = passwordBtn.getText();
+
+        System.out.println(id);
+        System.out.println(pwd);
+    }
+
+    public void registerBtnClick(){
+        String studentName = fullName.getText();
+        String fName = fatherName.getText();
+        String mName = motherName.getText();
+        String selectedSex = maleBtn.isSelected()?"male":"female";
+        String category = generalBtn.isSelected()?"general":"reservation";
+
+        System.out.println(studentName);
+        System.out.println(fName);
+        System.out.println(mName);
+        System.out.println(selectedSex);
+        System.out.println(category);
     }
 
     public void closeBtnClick(){
