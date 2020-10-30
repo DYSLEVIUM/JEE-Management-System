0cc85214dd4b33f316b28390a823032eda3dcdd8
Pushpakant Behera <pushpakantbehera@hotmail.com>
2020-10-30 12:48:59 +0545

    changed database location from absolute to relative in code

--- src/sample/databaseConnection.java
+++ src/sample/databaseConnection.java
@@ -1,13 +1,16 @@
 package sample;
 
 import javax.swing.*;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class databaseConnection {
+    private static File dbfile = new File("src\\res\\db");
+
     final static String JDBC_DRIVER = "org.sqlite.JDBC";
-    final static String JDBC_URL = "jdbc:sqlite:D:/dev/java/JEE-Management-System/src/res/db";
+    final static String JDBC_URL = "jdbc:sqlite:" + dbfile.getAbsolutePath();
 
     public static Connection connect(){
         Connection conn = null;
@@ -20,5 +23,3 @@ public class databaseConnection {
         return conn;
     }
 }
-
-
